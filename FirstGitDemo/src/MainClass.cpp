25a7619168e185089ccd3db5aca33d09dcd814a8
HP <HP@10.2.9.219>
2025-12-19 10:06:43 +0100

    Initial commit

---
+++ FirstGitDemo/src/MainClass.cpp
@@ -0,0 +1,17 @@
+/*
+ * MainClass.cpp
+ *
+ *  Created on: 19.12.2025
+ *      Author: HP
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Git Test" << endl;
+	return 0;
+}
+
+
